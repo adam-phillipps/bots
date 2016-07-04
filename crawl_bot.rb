4bb5a63dc73efd2eb7542c2b39716e66da8f2afe
adam-phillipps <adam.phillipps@gmail.com>
2016-07-04 10:16:14 -0600

    test crawl_bot

--- crawl_bot.rb
+++ crawl_bot.rb
@@ -6,6 +6,7 @@ class CrawlBot
   include Config
 
   def initialize
+    @run_time = rand(14400) + 7200 # random seconds from 2 to 6 hours
     poll
   end
 
@@ -18,44 +19,50 @@ class CrawlBot
         max_number_of_messages: 1,
         visibility_timeout: 10 # keep message invisible long enough to process to wip
       ) do |msg, stats|
-        byebug
         begin
-          # if JSON.parse(msg.body).has_key?('Records')
-          #   puts "\n\nPossible job found:\n\n#{JSON.parse(msg.body)}"
-          #   catch :no_such_job_in_backlog do
-          #     job = 'testing'
-          #     run_job(job)
-          #     puts "finished job:"
-          #   end
-          # else
-          puts "body: #{msg.body}"
-            sqs.delete_message({
+          byebug
+          body = JSON.parse(msg.body)
+          if possible_valid_job?(body)
+            puts "\n\nPossible job found:\n#{body}"
+            catch :no_such_job_in_backlog do
+              job = Job.new(msg, backlog_address)
+              job.run
+              puts "finished job: #{job.params}"
+            end
+          else
+            puts "body: #{msg.body}"
+            sqs.delete_message(
               queue_url: backlog_address,
               receipt_handle: msg.receipt_handle
-            })
-          # end
+            )
+          end
         rescue JSON::ParserError => e
           puts "Trouble with #{msg.body}:\n____#{e}\n"
         end
         puts 'Polling....'
       end
     end
-    ec2.terminate_instancers(ids: [self_id])
+    ec2.terminate_instances(ids: [self_id])
+  end
+
+  def possible_valid_job?(body)
+    !!(body.has_key?('crawler_product_result_id') &&
+        body.has_key?('title'))
   end
 
   def should_stop?
-    if hour_mark_approaches? ? death_ratio_acheived? : false
+    !!(time_is_up? ? death_ratio_acheived? : false)
   end
 
-  def hour_mark_approaches?
-    ((Time.now.to_i - boot_time) % 3600) > 3300
+  def time_is_up?
+    !!(@run_time - Time.now.to_i)
   end
 
   def death_ratio_acheived?
-    death_ratio >= death_threashold
+    !!(death_ratio >= death_threashold)
   end
 
-  def death_ratio #
+  def death_ratio
     backlog = get_count(backlog_address)
     wip = get_count(wip_address)
 
@@ -63,9 +70,10 @@ class CrawlBot
     backlog / wip
   end
 
-  def run_job(job)
-    sleep rand(polling_sleep_time)
-  end
+  # def run_job(job)
+  #   # system("java -jar google-scraper.jar #{job.params.join(' ')}")
+  #   puts "java -jar google-scraper.jar #{job.params.join(' ')}"
+  # end
 
   def self_id # hard code a value here for testing
     @id ||= 'fdasasfd' # HTTParty.get('http://169.254.169.254/latest/meta-data/instance-id')
--- job.rb
+++ job.rb
@@ -1,13 +1,12 @@
-require_relative 'config'
+require_relative './config'
 
 class Job
   include Config
   attr_reader :msg, :board
 
   def initialize(msg, board)
-    @msg = msg
+    @params = JSON.parse(msg.body)
     @board = board
-    File.delete(File.join(a_e_dir, 'Done')) if File.file?(File.join(a_e_dir, 'Done'))
   end
 
   def body
@@ -22,64 +21,34 @@ class Job
     end
   end
 
-  def create_elastic_transcoder_job(input_key, preset_id, output_key_prefix)
-    transcoder_client = Aws::ElasticTranscoder::Client.new(region: region, credentials: creds)
-    input = { key: input_key }
-    output = {
-      key: finished_key,
-      preset_id: preset_id
-    }
-
-    transcoder_client.create_job(
-      pipeline_id: pipeline_id,
-      input: input,
-      output_key_prefix: output_key_prefix,
-      outputs: [ output ]
-    )[:job][:id]
+  def params
+    [
+      @params['crawler_product_result_id'],
+      @params['title']
+    ]
   end
 
-  def delete_from_local_context
-    puts 'deleting from local'
-    FileUtils.rm_rf Dir.glob("#{a_e_file_dir_path}/*")
+  def run
+    # system("java -jar google-scraper.jar #{job.params.join(' ')}")
+    puts "java -jar google-scraper.jar #{job.params.join(' ')}"
   end
-
-  def delete_from_backlog_bucket
-    puts 'deleting from backlog bucket'
-    s3.delete_object(
-      bucket: 'backlog-pointway',
-      key: key
-    )
-  end
-
-  def done_file_exists?
-    File.file?(File.join(a_e_dir, 'Done'))
-  end
-
-  def a_e_file_dir_path
-    done_file_exists? ? finished : backlog
-  end
-
-  def file_path # fix name/path for windows
-    current_name = done_file_exists? ? finished_key : key
-    File.join(a_e_file_dir_path, current_name)
-  end
-
-  def finished_file_path
-    file_path.gsub(/\.\w{3}$/, '.mp4')
-  end
-
-  def key
-    body['Records'].first['s3']['object']['key']
-  end
-
-  def finished_key
-    key.gsub(/\.\w{3}$/, '.mp4')
-  end
-
-  def output_key_prefix
-    key.split('_').first + '/'
-  end
-
+# {
+#     "crawler_product_result_id" : 1,
+#   "type" : "type",
+#   "page_url" : "page_url",
+#   "title" : "title",
+#   "text" : "text",
+#   "brand" : "brand",
+#   "offer_price" : "offer_price",
+#   "regular_price" : "regular_price",
+#   "shipping_amount" : "shipping_amount",
+#   "save_amount" : "save_amount",
+#   "product_id" : "product_id",
+#   "sku" : "sku",
+#   "availability" : "availability",
+#   "size" : "size",
+#   "created_date" : created_date
+# }
   def next_board
     board == backlog_address ? wip_address : finished_address
   end
@@ -92,22 +61,6 @@ class Job
     msg.body
   end
 
-  def pull_file_from_backlog
-    puts 'pulling from backlog'
-    begin
-      resp = s3.get_object(
-        response_target: file_path,
-        bucket: 'backlog-pointway', # customer in
-        key: key
-      )
-
-      puts 'finished pulling'
-    rescue Aws::S3::Errors::NoSuchKey => e
-      puts "Couldn't find #{key} in backlog\n#{e.message}"
-      update_status(needs_attention_address)
-      throw :no_such_job_in_backlog
-    end
-  end
 
   def receipt_handle
     @receipt_handle = msg.receipt_handle
@@ -134,65 +87,6 @@ class Job
     end
   end
 
-  def unzip_file_and_unpack
-    unzip_file if file_path =~ /\.zip/
-  end
-
-  def unzip_file
-    puts 'unzipping file'
-    name = key.split('.').first
-
-    Zip::ZipFile.open(file_path) do |zip_file|
-     zip_file.each do |f|
-      extension = '.' + f.name.split('.').last
-      regulized_name = name + extension
-
-       f_path = File.join(backlog, regulized_name)
-       zip_file.extract(f, f_path) unless File.exist?(f_path)
-     end
-    end
-  end
-
-  def push_file
-    loop do
-      return push_file_to_video_in if done_file_exists?
-      sleep 3 # wait longer for the done file, if it doesn't exist yet
-    end
-  end
-
-  def push_file_to_video_in
-    puts 'pushing to finished bucket'
-    resp = ''
-    location = Dir.glob("#{finished}/*").first
-    File.open(location, 'rb') do |file|
-      puts "Pushing file:\n#{finished_file_path}\n"
-      resp = s3.put_object(bucket: video_in, key: finished_key, body: file)
-      file.close
-    end
-    resp
-  end
-
-  def signal_a_e_to_start
-    puts 'signaling ae to start'
-    File.delete(File.join(a_e_dir, 'Done')) if done_file_exists?
-    f = File.new(File.join(a_e_dir, 'Go'), 'w')
-    f.close
-  end
-
-  def transcode_from_video_in
-    puts 'transcode job started...'
-      transcode_job_id = create_elastic_transcoder_job(finished_key, preset_id, output_key_prefix)
-    puts transcode_job_id
-  end
-
-  def clean_up_for_next_job
-    delete_from_local_context
-    File.delete(File.join(a_e_dir, 'Done')) if File.file?(File.join(a_e_dir, 'Done'))
-    delete_from_backlog_queue
-    delete_from_backlog_bucket
-    delete_from_wip_queue
-    delete_from_local_context
-  end
 
   def delete_from_wip_queue
     puts 'deleting from wip queue'
